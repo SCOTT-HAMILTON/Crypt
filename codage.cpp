ba436d73c422d853f8060904deae1f08a8fec9d7
SCOTT-HAMILTON <sgn.hamilton@gmail.com>
2016-12-13 09:35:16 +0100

    release

---
+++ codage.cpp
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdio.h>
+
+void decodage(void){
+	printf("Entre le mot a coder\n");
+	char texte[50];
+	scanf("%s",&texte);
+	char clef[20];
+	printf("Entre la clef de codage:\n");
+	scanf("%s",clef);
+	int i = 0;int taille = strlen(texte);
+	int k = 0;int taille_de_clef = strlen(clef);
+	/*.....................................*/
+	int solution = 0;
+	
+	for(i = 0;i <= taille; i++){
+		k = clef[i];
+			if(i > taille_de_clef){
+		k = clef[0];
+		}
+		texte[i]-=k;
+		if(texte[i] < 32){
+			texte[i] +=128;
+		}
+		solution++;
+		if(solution != taille){
+		printf("%c",texte[i]);
+		}
+	}
+
+   
+    /*.....................................*/
+	FILE *fichier = NULL;
+	fichier = fopen("source.txt","w");
+	fprintf(fichier,"%s",texte);
+	fclose(fichier);
+int arret;
+	scanf("%d",&arret);
+}
